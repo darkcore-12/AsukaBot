8d511a1b2e8b5bf688858db4d61aba10aaa34f78
DarkCore <darkcore12g@gmail.com>
2025-05-01 20:07:08 -0500

    Update ai-gemini.js

--- plugins/ai-gemini.js
+++ plugins/ai-gemini.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 var handler = async (m, { text,  usedPrefix, command }) => {
-if (!text) return conn.reply(m.chat, `${emoji} Ingrese una petición para que Gemini lo responda.`, m)
+if (!text) return conn.reply(m.chat, `${emoji} 🌸ꗥ～𝐈𝐧𝐠𝐫𝐞𝐬𝐞 𝐮𝐧𝐚 𝐩𝐞𝐭𝐢𝐜𝐢ó𝐧 𝐩𝐚𝐫𝐚 𝐪𝐮𝐞 𝐆𝐞𝐦𝐢𝐧𝐢 𝐥𝐨 𝐫𝐞𝐬𝐩𝐨𝐧𝐝𝐚～ꗥ🌸`, m)
 try {
 await m.react(rwait)
 conn.sendPresenceUpdate('composing', m.chat)
@@ -9,7 +9,7 @@ var res = await apii.json()
 await m.reply(res.result)
 } catch {
 await m.react('❌')
-await conn.reply(m.chat, `${msm} Gemini no puede responder a esa pregunta.`, m)
+await conn.reply(m.chat, `${msm} 🌸ꗥ～𝐆𝐞𝐦𝐢𝐧𝐢 𝐧𝐨 𝐩𝐮𝐞𝐝𝐞 𝐫𝐞𝐬𝐩𝐨𝐧𝐝𝐞𝐫 𝐚 𝐞𝐬𝐚 𝐩𝐫𝐞𝐠𝐮𝐧𝐭𝐚～ꗥ🌸`, m)
 }}
 handler.command = ['gemini']
 handler.help = ['gemini']
