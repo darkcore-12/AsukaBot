f8ba2cbaf9c88b9545a68437b5872703db96324e
DarkCore <darkcore12g@gmail.com>
2025-05-02 18:01:12 -0500

    Update _antisubbots.js

--- plugins/_antisubbots.js
+++ plugins/_antisubbots.js
@@ -1,25 +1,23 @@
 import { areJidsSameUser } from '@whiskeysockets/baileys'
+
 export async function before(m, { participants, conn }) {
     if (m.isGroup) {
         let chat = global.db.data.chats[m.chat];
 
-         if (!chat.antiBot2) {
+        if (!chat.antiBot2) {
             return
         }
 
-
         let botJid = global.conn.user.jid // JID del bot principal
 
-       if (botJid === conn.user.jid) {
-           return
+        if (botJid === conn.user.jid) {
+            return
         } else {
-           let isBotPresent = participants.some(p => areJidsSameUser(botJid, p.id))
+            let isBotPresent = participants.some(p => areJidsSameUser(botJid, p.id))
 
-          if (isBotPresent) {
-                setTimeout(async () => {
-                    await conn.reply(m.chat, `╭━〔 ⚔ 𝐊𝐈𝐑𝐈𝐓𝐎-𝐁𝐎𝐓 ⚔ 〕━╮\n┃ En este grupo ya está el bot principal.\n┃ Me retiraré para evitar spam.\n╰━━━━━━━━━━━━━━━━━━━╯`, m, rcanal);
-                    await this.groupLeave(m.chat)
-                }, 5000)// 5 segundos
+            if (isBotPresent) {
+                // Si otro bot está en el grupo, no responder
+                //return m.reply('🤖 Ya hay otro bot en este grupo, no puedo responder.')
             }
         }
     }
